11ce0d19e11e2cbf010bd9e5df47516cee89e674
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-09-05 00:49:14 -0400

    finalized javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,56 @@
+//declare all needed variables
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
 var wins = 0;
-
 var losses = 0;
-
 var guesses = 9;
-
 var guessesLeft = 9;
-
-var guessedLetters = [];
-
+var lettersGuessed = [];
+var newLetter = null;
 var compChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+//define functions
+var addLettersGuessed = function() {  
+	document.querySelector('#lettersGuessed').innerHTML = lettersGuessed.join(', ');
+};
+
+var updateGuessesLeft = function() {
+	document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+};
+
+var updateCompChoice = function() {
+	compChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+};
+
+var reset = function() {
+	totalGuesses = 9;
+	guessesLeft = 9;
+	lettersGuessed = [];
+	document.querySelector("#lettersGuessed").innerHTML = lettersGuessed;
+
+	updateCompChoice();
+	updateGuessesLeft();
+	addLettersGuessed();
+};
+
+//function runs when user makes a guess
 document.onkeyup = function(event) {
-
-	var userGuess = event.key;
-
-	document.getElementById("guessSoFar").innerHTML = userGuess;
-
-	document.getElementById("guesses").innerHTML = guesses--;
-
+	guessesLeft--;
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	lettersGuessed.push(userGuess);
+	updateGuessesLeft();
+	addLettersGuessed();
+	
+	if (guessesLeft > 0) {
+		if (userGuess == compChoice) {
+			wins++;
+			document.getElementById("wins").innerHTML = wins;
+			alert("You guessed it!");
+			reset();
+		} 
+	} else if (guessesLeft == 0) {
+        losses++;
+        document.querySelector('#losses').innerHTML = losses;
+        alert("You're clearly not a psychic.");
+        reset();
+        }
 }
